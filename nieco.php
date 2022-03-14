2a64ca478a6458d3fd231d92949e9bdb725030b5
benjoni <maxulak77@gmail.com>
2022-03-14 11:12:46 +0100

    pridal som do nieco div

--- nieco.php
+++ nieco.php
@@ -1,4 +1,12 @@
 <?php
 echo"toto je nieco";
 $a=2666;
+
+<div class="col-5 ">
+                                                <div class="form-group">
+                                                    <div class="controls">
+                                                        <label>Synopsis</label>
+                                                        <input type="text" name="synopsis" class="form-control" value="<?php echo $synopsis ?>" >
+                                                    </div>
+                                                </div>
 ?>
